594214c00fb63e5796c963a37b3401bfdf0dba74
sjukdom <svrt.hlvt@gmail.com>
2019-04-28 18:18:40 -0500

    Aplicacion web principal

--- src/client/App.js
+++ src/client/App.js
@@ -2,15 +2,35 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Navbar from './Components/Navbar';
+import Player from './Components/Player';
+import UserContent from './Components/UserContent';
+import ArtistContent from './Components/ArtistContent';
+import {withStyles,
+        Grid, 
+        Paper} from '@material-ui/core';
 
 class App extends React.Component {
         render() {
                 return(
                         <div>
-                                <Navbar items={7}/>
+                                <Grid container spacing={8}> 
+                                        <Grid item xs={12}>
+                                                <Navbar items={10} />
+                                        </Grid>
+                                        <Grid item xs={4.5}>
+                                                <UserContent/>
+                                        </Grid>
+                                        <Grid item xs>
+                                                <ArtistContent/>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                                <Player song_info={'Kase.O, Como el sol'}/>
+                                        </Grid>
+                                </Grid>
                         </div>
                 );
         }
 }
+
 
 render(<App />, document.getElementById("app"));
