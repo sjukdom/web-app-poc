979bbfc0fcab050e71c0013ac9cc78320f34ed31
sjukdom <svrt.hlvt@gmail.com>
2019-05-09 18:30:57 -0500

    Added more functions to artist content

--- src/client/Components/ArtistContent.js
+++ src/client/Components/ArtistContent.js
@@ -2,32 +2,48 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Paper, withStyles} from '@material-ui/core';
+import AlbumGridList from './AlbumGridList';
 
 const styles = {
     root: {
       width: 'auto',
-      height: '640px'
+      height: '640px',
+      overflow: 'scroll'
     },
     Paper: {
             padding: 220,
             marginTop: 10,
             marginBottom: 10,
+            heigh: '100%'
     },
 }
 
-class UserContent extends React.Component {
+// class ArtistContent extends React.Component {
+//     render() {
+//
+//       const {classes} = this.props;
+//
+//             return(
+//                     <div className={classes.root}>
+//                         <Paper className={classes.Paper}>
+//                             Right pane
+//                         </Paper>
+//                     </div>
+//             );
+//     }
+// }
+
+class ArtistContent extends React.Component {
     render() {
 
       const {classes} = this.props;
 
             return(
                     <div className={classes.root}>
-                        <Paper className={classes.Paper}>
-                            Right pane
-                        </Paper>
+                      <AlbumGridList searchData={[{id:0, data: 'aue'}, {id: 1, data:'2323'}, {id:2,data:'22'},{id:3,data:'232'}]}/>
                     </div>
             );
     }
 }
 
-export default withStyles(styles)(UserContent);
+export default withStyles(styles)(ArtistContent);
